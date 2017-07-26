7d78c7fb5f146898c10ea559444a62ef653761c5
fan jianping <fan jianping>
2017-07-26 23:26:36 +0800

    blog.sql add table user

---
+++ blog.sql
@@ -0,0 +1,20 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2017-07-26 23:24:29
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+#
+# Data for table "user"
+#
+
