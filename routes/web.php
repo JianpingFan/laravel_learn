b0f7cff31a5fb51a89fc33ed266f1a60784dc7cf
fan jianping <fan jianping>
2017-07-30 18:01:11 +0800

    captcha;

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,28 @@ Route::group(['middleware' => 'web'], function () {
     });
 
     Route::get('admin/login',"Admin\LoginController@login");
+
+    Route::get('admin/captcha',function(){
+        if (Request::getMethod() == 'POST')
+        {
+            $rules = ['captcha' => 'required|captcha'];
+            $validator = Validator::make(Input::all(), $rules);
+            if ($validator->fails())
+            {
+                echo '<p style="color: #ff0000;">Incorrect!</p>';
+            }
+            else
+            {
+                echo '<p style="color: #00ff30;">Matched :)</p>';
+            }
+        }
+
+        $form = '<form method="post" action="captcha-test">';
+        $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+        $form .= '<p>' . captcha_img() . '</p>';
+        $form .= '<p><input type="text" name="captcha"></p>';
+        $form .= '<p><button type="submit" name="check">Check</button></p>';
+        $form .= '</form>';
+        return $form;
+    });
 });
